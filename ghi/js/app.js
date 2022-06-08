1075da5090c7ec351e81c154edc4f9b35773e09c
Alex Chiu <alexdchiu@gmail.com>
2022-06-07 20:41:20 -0400

    completed first fetch, data to DOM and show the image

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,45 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 
+  const url = 'http://localhost:8000/api/conferences/';
+  
+  try {
+    const response = await fetch(url);
+    // console.log(response);
+
+    if (!response.ok) {
+      // figure out what to do when the response is bad
+      alert({
+        "code": 400,
+        "message": "Invalid JSON Response Received"
+      })
+    } else {
+      const data = await response.json()
+      // console.log(data)
+
+      const conference = data.conferences[0]
+      // console.log(conference)
+      const nameTag = document.querySelector('.card-title')
+      nameTag.innerHTML = conference.name
+
+      const detailUrl = `http://localhost:8000${conference.href}`
+      const detailResponse = await fetch(detailUrl)
+      if (detailResponse.ok) {
+        const details = await detailResponse.json()
+        // console.log(details)
+        // console.log(details.conference.description)
+        const confDescription = details.conference.description
+        const confDescriptTag = document.querySelector('.card-text')
+        confDescriptTag.innerHTML = confDescription
+
+        const confImageUrl = details.conference.location.picture_url
+        const confImageTag = document.querySelector('.card-img-top')
+        confImageTag.src = confImageUrl
+      }
+    }
+    
+  } catch (e) {
+    // figure out what to do if an error is raised
+    alert(e.name, e.message)
+  }
 
 });
